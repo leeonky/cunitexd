763c61513517a7a3365a573893bd4d910abdc15f
leeonky <leeonky@gmail.com>
2017-05-22 11:52:39 +0000

    merge from testutil

--- cunitexd.c
+++ cunitexd.c
@@ -1,1 +1,70 @@
+#include <stdlib.h>
+#include <stdarg.h>
 #include "cunitexd.h"
+
+app_context actxt;
+
+void init_app_context(app_context *context, const char *input, char *arg1, ...) {
+	va_list list;
+	context->argc = 0;
+	context->argv[context->argc++] = "main";
+	va_start(list, arg1);
+	while(strlen(arg1)>0) {
+		context->argv[context->argc++] = arg1;
+		arg1 = va_arg(list, char *);
+	}
+	va_end(list);
+
+	strncpy(context->input_buffer, input, sizeof(context->input_buffer));
+	context->input_stream = fmemopen(context->input_buffer, sizeof(context->input_buffer), "r");
+	context->output_stream = fmemopen(context->output_buffer, sizeof(context->output_buffer), "w");
+	context->error_stream = fmemopen(context->error_buffer, sizeof(context->error_buffer), "w");
+}
+
+char* output_buffer(app_context *context) {
+	fflush(context->output_stream);
+	return context->output_buffer;
+}
+
+char* error_buffer(app_context *context) {
+	fflush(context->error_stream);
+	return context->error_buffer;
+}
+
+void close_app_context(app_context *context) {
+	fclose(context->input_stream);
+	fclose(context->output_stream);
+	fclose(context->error_stream);
+}
+
+int invoke_app(app_context *ctxt, int(*sub_main)(int, char**, FILE *, FILE *, FILE *)){
+	return sub_main(ctxt->argc, ctxt->argv, ctxt->input_stream, ctxt->output_stream, ctxt->error_stream);
+}
+
+void init_test() {
+	if (CUE_SUCCESS != CU_initialize_registry())
+		exit(CU_get_error());
+}
+
+int run_test() {
+	CU_basic_set_mode(CU_BRM_VERBOSE);
+	CU_basic_run_tests();
+	CU_cleanup_registry();
+	return CU_get_error();
+}
+
+CU_pSuite create_suite(const char *suit_name, int (*init)(), int (*clean)()) {
+	CU_pSuite suite = CU_add_suite(suit_name, init, clean);
+	if (NULL == suite) {
+		CU_cleanup_registry();
+		exit(CU_get_error());
+	}
+	return suite;
+}
+
+void add_case_with_name(CU_pSuite suite, const char *case_name, void (*test)()) {
+	if (NULL == CU_add_test(suite, case_name, test)) {
+		CU_cleanup_registry();
+		exit(CU_get_error());
+	}
+}
--- cunitexd.h
+++ cunitexd.h
@@ -4,4 +4,122 @@
 #include <CUnit/Basic.h>
 #include <stdio.h>
 
+typedef struct app_context {
+	char input_buffer[1024];
+	char output_buffer[1024];
+	char error_buffer[1024];
+	FILE *input_stream;
+	FILE *output_stream;
+	FILE *error_stream;
+	int argc;
+	char *argv[64];
+} app_context;
+
+extern void init_app_context(app_context *, const char *, char *, ...);
+extern char* output_buffer(app_context *);
+extern char* error_buffer(app_context *);
+extern void close_app_context(app_context *);
+extern int invoke_app(app_context *, int(*)(int, char**, FILE *, FILE *, FILE *));
+
+extern app_context actxt;
+
+#define init_subject(input, ...) init_app_context(&actxt, input, ##__VA_ARGS__)
+#define std_out output_buffer(&actxt);
+#define std_err error_buffer(&actxt);
+#define close_subject() close_app_context(&actxt)
+#define invoke_subject(main_func) invoke_app(&actxt, main_func)
+
+
+extern void init_test();
+extern int run_test();
+extern CU_pSuite create_suite(const char *suit_name, int (*init)(), int (*clean)());
+extern void add_case_with_name(CU_pSuite suite, const char *case_name, void (*test)());
+
+#define add_case(suite, test_case) add_case_with_name(suite, #test_case, test_case)
+
+#define extern_mock_void_function_0(func) \
+	extern int func ## _times;\
+	extern void (*mock_ ## func)();
+#define mock_void_function_0(func) \
+	int func ## _times;\
+	void (*mock_ ## func)();\
+	void func() {\
+		++func ## _times;\
+		if(mock_ ## func)\
+			mock_ ## func();\
+	}
+
+#define extern_mock_void_function_1(func, t1) \
+	extern int func ## _times;\
+	extern void (*mock_ ## func)(t1);\
+	extern t1 func ## _p1;
+#define mock_void_function_1(func, t1) \
+	int func ## _times;\
+	t1 func ## _p1;\
+	void (*mock_ ## func)(t1);\
+	void func(t1 p1) {\
+		++func ## _times;\
+		func ## _p1 = p1;\
+		if(mock_ ## func)\
+			mock_ ## func(p1);\
+	}
+
+#define extern_mock_function_2(rtype, func, t1, t2) \
+	extern int func ## _times;\
+	extern rtype (*mock_ ## func)(t1, t2);\
+	extern t1 func ## _p1;\
+	extern t2 func ## _p2;
+#define mock_function_2(rtype, func, t1, t2) \
+	int func ## _times;\
+	t1 func ## _p1;\
+	t2 func ## _p2;\
+	rtype (*mock_ ## func)(t1, t2);\
+	rtype func(t1 p1, t2 p2) {\
+		static rtype default_result;\
+		++func ## _times;\
+		func ## _p1 = p1;\
+		func ## _p2 = p2;\
+		if(mock_ ## func)\
+			return mock_ ## func(p1, p2);\
+		return default_result;\
+	}
+
+#define extern_mock_function_4(rtype, func, t1, t2, t3, t4) \
+	extern int func ## _times;\
+	extern rtype (*mock_ ## func)(t1, t2, t3, t4);\
+	extern t1 func ## _p1;\
+	extern t2 func ## _p2;\
+	extern t3 func ## _p3;\
+	extern t4 func ## _p4;
+#define mock_function_4(rtype, func, t1, t2, t3, t4) \
+	int func ## _times;\
+	t1 func ## _p1;\
+	t2 func ## _p2;\
+	t3 func ## _p3;\
+	t4 func ## _p4;\
+	rtype (*mock_ ## func)(t1, t2, t3, t4);\
+	rtype func(t1 p1, t2 p2, t3 p3, t4 p4) {\
+		static rtype default_result;\
+		++func ## _times;\
+		func ## _p1 = p1;\
+		func ## _p2 = p2;\
+		func ## _p3 = p3;\
+		func ## _p4 = p4;\
+		if(mock_ ## func)\
+			return mock_ ## func(p1, p2, p3, p4);\
+		return default_result;\
+	}
+
+#define called_times_of(func) (func ## _times)
+
+#define params_of(func, at) (func ## _p ## at)
+
+#define init_mock_function(func, stub) \
+	func ## _times = 0;\
+	mock_ ## func = stub;
+
+#define CU_EXPECT_CALLED_ONCE(func) CU_ASSERT_EQUAL(called_times_of(func), 1)
+#define CU_EXPECT_CALLED_WITH_STRING(func, at, arg) CU_ASSERT_STRING_EQUAL(params_of(func, at), arg)
+#define CU_EXPECT_CALLED_WITH(func, at, arg) CU_ASSERT_EQUAL(params_of(func, at), arg)
+
 #endif
